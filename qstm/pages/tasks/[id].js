dec913f222cc6ae1e970f6c141e0af64c42276a8
Iris Leal <iris_leal_m@hotmail.com>
2020-07-15 07:19:58 -0700

    Now getting the data from the specified fiels

--- qstm/pages/tasks/[id].js
+++ qstm/pages/tasks/[id].js
@@ -2,9 +2,9 @@ import { useRouter } from 'next/router'
 import axios from 'axios'
 
 
-export default function CourseDetail(props) {
-    const url = 'https://ileal-summer-camp-api.herokuapp.com/api/v1/courses/';
-    
+export default function TaskDetail(props) {
+    // const url = 'https://ileal-summer-camp-api.herokuapp.com/api/v1/courses/';
+    // const tasks_url = 'http://ec2-18-191-129-83.us-east-2.compute.amazonaws.com/api/v1/tasks/';
 
     const router = useRouter();
 
@@ -17,20 +17,20 @@ export default function CourseDetail(props) {
 
     return (
         <>
-        <h1>I am a single course </h1>
+        <h1>I am a single task {props.task.description} </h1>
         {/* <button onClick={() => deleteHandler(props.course.id)}>Delete</button> */}
         </>
     )
 }
 
 export async function getServerSideProps(context) {
-    console.log('entering getServerSideProps')
-    // const response = await fetch(`https://ileal-summer-camp-api.herokuapp.com/api/v1/courses/${context.params.id}`);
-    // const course = await response.json();
-    // console.log('course',course)
+    // console.log('entering getServerSideProps....:',context.params.id)
+    const response = await fetch(`http://ec2-18-191-129-83.us-east-2.compute.amazonaws.com/api/v1/tasks/${context.params.id}`);
+    const task = await response.json();
+    console.log('task',task)
     return {
         props: {
-            course:'sdf',
+          task: task,
         }
     }
 }
