e27c0225e5b3ee42e7b6b758f1074acdfb9e32fc
Iris Leal <iris_leal_m@hotmail.com>
2020-07-16 08:29:56 -0700

    Added due date to the task component

--- qstm/components/tasks/Task.js
+++ qstm/components/tasks/Task.js
@@ -18,6 +18,7 @@ export default function Task(props) {
             <ul>
               <li> <b> Class topic: </b>  {props.task.class_topic} </li>  
               <li> <b> Date created: </b>  {props.task.date_created} </li>  
+              <li> <b> Due date: </b>  {props.task.due_date} </li>  
               <li> <b> Priority: </b>  {props.task.priority} </li>  
               <li> <b> Completed: </b> { String( props.task.completed)} </li>  
               <li> <b> Date completed: </b>  {props.task.date_completed} </li>  
