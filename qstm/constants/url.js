b2b1bc0bed5956e6554a9aafafb8a96a48546681
joseph-zabaleta <joseph.l.zabaleta@gmail.com>
2020-07-15 21:31:12 -0700

    Testing Environment ACP

--- qstm/constants/url.js
+++ qstm/constants/url.js
@@ -1,5 +1,6 @@
 export default {
-    BASE: 'http://ec2-18-191-129-83.us-east-2.compute.amazonaws.com/api/v1/',
+    // BASE: 'http://ec2-18-191-129-83.us-east-2.compute.amazonaws.com/api/v1/',
+    BASE: 'http://127.0.0.1:8000/api/v1/',
     STUDENT: 'students/',
     PARENT: 'parents/',
     SITE: 'sites/',
--- qstm/utils/studentSites.js
+++ qstm/utils/studentSites.js
@@ -1,7 +1,9 @@
 
+import ApiUrl from '../constants/url';
+
 
 export async function getSitesByStudentId(id) {
-    const url = `http://ec2-18-191-129-83.us-east-2.compute.amazonaws.com/api/v1/sites/?student_id=${id}`
+    const url = ApiUrl.BASE + ApiUrl.SITE + `?student_id=${id}`
 
     const response = await fetch(url);
     const data = await response.json()
